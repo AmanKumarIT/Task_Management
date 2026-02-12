b2a7a490714a28a9179c10b0da7d343844ed0499
AmanKumarIT <akpailwar120@gmail.com>
2026-02-12 21:45:19 +0530

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://task-management-17b7.onrender.com"
+  baseURL: "https://task-management-iqde.onrender.com"
 });
 
 instance.interceptors.request.use((config) => {
